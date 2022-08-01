be75c050188781fab16e1702b7fe52172f95e577
sholom sero <sholom.serebryanski@webdocs.com.au>
2022-08-02 08:51:48 +1000

    remove unused file

--- c-sharp-playground-API/ApiPlayground/regenerate-controller-from-swagger.sh
+++
@@ -1,2 +0,0 @@
-docker pull asia.gcr.io/glx-devops-au/docker-nswag:28
-docker run --rm -u ${UID}:${GROUPS} -v ${PWD}/templates:/templates -v ${PWD}:/src -w /src asia.gcr.io/glx-devops-au/docker-nswag:28 run ./nswag.json /runtime:Net60
